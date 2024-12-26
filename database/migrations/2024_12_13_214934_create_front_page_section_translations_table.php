e3943b702ef1e2ebe9874ebbabdf09528b43e5f4
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2024-12-26 02:24:26 +0200

    tested dashboard

--- database/migrations/2024_12_13_214934_create_front_page_section_translations_table.php
+++ database/migrations/2024_12_13_214934_create_front_page_section_translations_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('front_page_section_translations', function (Blueprint $table) {
             $table->id();
             $table->text('content')->nullable();
-            $table->unsignedBigInteger('front_page_sections_id'); // Add event_id column
+            $table->unsignedBigInteger('front_page_section_id'); // Add event_id column
             $table->string('locale');               // Add locale column for uniqueness constraint
             $table->unique(['front_page_section_id', 'locale'], 'fp_section_id_locale_unique');
             $table->foreign('front_page_section_id')->references('id')->on('front_page_sections')->onDelete('cascade');
