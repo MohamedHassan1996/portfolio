9273051de96723130a2d8d43e0634548ae4fcd59
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-01-12 01:39:26 +0200

    update

--- resources/views/Layout/navbar.blade.php
+++ resources/views/Layout/navbar.blade.php
@@ -18,7 +18,7 @@
             <!-- Navigation Links -->
             <div class="nav-links">
                 @foreach ($navbarLinks as $navbarLink)
-                    @if ($navbarLink->controller_name != 'ContactPageController')
+                    @if ($navbarLink->controller_name != 'ContactPageController' || $navbarLink->controller_name != 'CareerPageController')
                         <a href="{{ route('dynamic.page', ['lang' => app()->getLocale() == 'en' ? '':app()->getLocale(), 'slug' => $navbarLink->slug]) }}"
                            class="nav-link {{ session('active_navbar_link') === $navbarLink->slug ? 'active' : '' }}">
                             {{ $navbarLink->title }}
