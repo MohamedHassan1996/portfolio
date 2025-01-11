3f456afbe5cc5c3fc306aaab52886a23dd189f48
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-01-12 00:59:56 +0200

    update

--- resources/views/Layout/header.blade.php
+++ resources/views/Layout/header.blade.php
@@ -3,8 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
-    {{-- @vite(['resources/css/app.css']) --}}
+    @vite(['resources/css/app.css'])
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700" />
