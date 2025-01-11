8ed71cb7ff4d1cf27b2f84aec007b1dd6b05f0b3
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-01-12 01:33:45 +0200

    update

--- resources/views/Layout/footer.blade.php
+++ resources/views/Layout/footer.blade.php
@@ -26,14 +26,14 @@
         </div>
 
         <div class="flex flex-col gap-[16px] w-[100px]">
-            @foreach ($navbarLinks as $navbarLink)
+            {{-- @foreach ($navbarLinks as $navbarLink)
                     @if ($navbarLink->controller_name != 'ContactPageController')
                         <a href="{{ route('home.index', ['lang' => app()->getLocale() == 'en' ? '':app()->getLocale(), 'slug' => $navbarLink->slug]) }}"
                            class="[font-family:'Open_Sans',sans-serif] text-[18px] text-base font-normal leading-4 text-[#FBFCF8] cursor-pointer {{ session('active_navbar_link') === $navbarLink->slug ? 'active' : '' }}">
                             {{ $navbarLink->title }}
                         </a>
                     @endif
-                @endforeach
+                @endforeach --}}
         </div>
 
         <div class="SubscribeForm w-[450px] sm:mb-[250px]">
