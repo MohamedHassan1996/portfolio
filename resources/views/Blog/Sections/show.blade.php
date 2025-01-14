4685231c85f2d8d8e361a8a63f79dd7cfbe27b93
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-01-14 12:17:46 +0200

    update client

--- resources/views/Blog/Sections/show.blade.php
+++ resources/views/Blog/Sections/show.blade.php
@@ -25,7 +25,7 @@
             <div class="information md:w-[80%]">
               <p class="main-title [font-family:Montserrat,sans-serif] text-6xl font-bold leading-[72px] uppercase text-[#333333]">{{ $blog->title }}</p>
               <p class="[font-family:Roboto,sans-serif] text-xl font-normal text-left leading-[30px] text-[#777777] w-full box-border mt-10">
-                {{ $blog->content }}
+                {{!! $blog->content !!}}
               </p>
             </div>
 
