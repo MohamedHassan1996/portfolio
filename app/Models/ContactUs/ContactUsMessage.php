dcda7a2caced43529f84a40c8cccb3c919195f55
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-01-29 18:10:28 +0200

    u

--- app/Models/ContactUs/ContactUsMessage.php
+++ app/Models/ContactUs/ContactUsMessage.php
@@ -16,7 +16,7 @@ class ContactUsMessage extends Model
         'message',
         'is_read',
         'is_admin',
-        'contact_us_Id',
+        'contact_us_id',
     ];
 
     protected $casts = [
