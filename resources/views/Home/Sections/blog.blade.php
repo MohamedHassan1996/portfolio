6398759df2c0947a4d8e785d4f667f9c18b1f7d0
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-02-08 19:05:30 +0200

    update string path in section images

--- resources/views/Home/Sections/blog.blade.php
+++ resources/views/Home/Sections/blog.blade.php
@@ -12,7 +12,7 @@
         <div class="bg-[rgba(51,51,51,0.04)] max-h-[500px]  relative box-border flex justify-start items-stretch flex-col rounded-lg">
             <div class="box-border flex justify-start items-stretch flex-col rounded-lg h-[300px]">
               <img src="{{ url("public/storage/".$blog->thumbnail) }}" class="h-full w-full" alt="" loading="lazy" />
-              <div class="bg-[white] absolute top-3 right-3 box-border flex justify-start items-center flex-col px-2 rounded-lg">
+              <div class="bg-[white] absolute top-3 right-3 box-border px-[15px] py-[7px] flex-col px-2 rounded-lg">
                 <p class="[font-family:Montserrat,sans-serif] text-2xl font-semibold text-[#333333] -mb-2">{{ $blog->created_at->format('d') }}</p>
                 <p class="[font-family:Roboto,sans-serif] text-sm font-normal text-[#333333]">{{ $blog->created_at->format('M') }}</p>
               </div>
