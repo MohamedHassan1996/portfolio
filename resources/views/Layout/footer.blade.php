79a8b885c13c9ccfc5d4a072be6cc8c4e5a50aad
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-02-08 20:15:40 +0200

    update string path in section images

--- resources/views/Layout/footer.blade.php
+++ resources/views/Layout/footer.blade.php
@@ -21,6 +21,7 @@
                 <bdi>010 69166696</bdi>
             </a>
             <br>
+            @dd($mainSettings)
             <a href="mailto:{{ $mainSettings->content->emails[0] }}" class="text-[#FBFCF8] font-[100] mt-1">
                 {{ $mainSettings->content->emails[0] }}
             </a>
