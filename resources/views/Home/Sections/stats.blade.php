da5de4da6916c34b53ebd5a49401e26ad426623e
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-02-09 17:22:14 +0200

    m

--- resources/views/Home/Sections/stats.blade.php
+++ resources/views/Home/Sections/stats.blade.php
@@ -1,12 +1,13 @@
 <!-- status numbres -->
 <section class="bg-[#f5f5f3]">
-    @dd($statsSection->content)
     <div class="w-4/5 sm:w-4/5 m-auto flex items-center flex-col lg:flex-row box-border justify-between gap-[50px] py-12">
         @foreach ($statsSection->content as $index => $stat)
       <div class="flex justify-center items-center flex-col">
         <p
           class="stats-counter [font-family:Montserrat,sans-serif] text-[56px] font-bold leading-[67px] text-[#166e1d]"
         >
+        @dd($stat, $stat['number'])
+
           {{ $stat['number'] }}
         </p>
         <p
