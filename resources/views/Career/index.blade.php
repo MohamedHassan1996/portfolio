6d2f4e3d5dc3bf1f0f384f8d3cb44752b7ca4813
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-02-16 11:23:35 +0200

    activate career

--- resources/views/Career/index.blade.php
+++ resources/views/Career/index.blade.php
@@ -26,68 +26,12 @@
               </p>
               @foreach ($careers as $career)
               <div class="grid gap-8 mt-12 justify-center grid-cols-[repeat(auto-fill,minmax(300px,1fr))] md:grid-cols-[repeat(auto-fill,minmax(400px,1fr))]">
-                <a href="./career-details.html" class="career-box-content cursor-pointer border bg-[white] box-border flex justify-center items-stretch flex-col gap-4 max-w-[600px] px-[31px] py-8 rounded-lg border-solid border-[#e8e8e8]"
+                <a href="{{ url()->current()}}/{{ $career->slug }}" class="career-box-content cursor-pointer border bg-[white] box-border flex justify-center items-stretch flex-col gap-4 max-w-[600px] px-[31px] py-8 rounded-lg border-solid border-[#e8e8e8]"
                 >
                   <p
                     class="[font-family:Montserrat,sans-serif] text-[35px] font-semibold leading-[42px] uppercase text-[#333333]"
                   >
                     {{ $career->title }}
-                  </p>
-                  <p
-                    class="[font-family:'Open_Sans',sans-serif] text-[17px] font-normal text-left leading-[23px] text-[#8e8e8e]"
-                  >
-                    {{ $career->description }}
-                  </p>
-                  <div
-                    class="flex justify-start items-center flex-row grow-0 shrink-0 basis-auto"
-                  >
-                    <button
-                      class="bg-[rgba(51,51,51,0.04)] [font-family:Roboto,sans-serif] text-base font-normal text-[#333333] cursor-pointer min-w-[109px] h-8 w-[109px] rounded-[999px] border-[none]"
-                    >
-                      Full time
-                    </button>
-                    <button
-                      class="bg-[rgba(51,51,51,0.04)] [font-family:Roboto,sans-serif] text-base font-normal text-[#333333] cursor-pointer min-w-[149px] h-8 w-[149px] ml-4 rounded-[999px] border-[none]"
-                    >
-                      +2 Experience
-                    </button>
-                  </div>
-                </a>
-                <a href="./career-details.html" class="career-box-content cursor-pointer border bg-[white] box-border flex justify-center items-stretch flex-col gap-4 max-w-[600px] px-[31px] py-8 rounded-lg border-solid border-[#e8e8e8]">
-                  <p
-                    class="[font-family:Montserrat,sans-serif] text-[35px] font-semibold leading-[42px] uppercase text-[#333333]"
-                  >
-                    product manager
-                  </p>
-                  <p
-                    class="[font-family:'Open_Sans',sans-serif] text-[17px] font-normal text-left leading-[23px] text-[#8e8e8e]"
-                  >
-                    Qui veritatis quo non nostrum eveniet consequatur animi
-                    voluptas<br />
-                    etQui veritatis quo non nostrum eveniet consequaturQui veritatis
-                    quo non nostrum eveniet consequatur animi
-                  </p>
-                  <div
-                    class="flex justify-start items-center flex-row grow-0 shrink-0 basis-auto"
-                  >
-                    <button
-                      class="bg-[rgba(51,51,51,0.04)] [font-family:Roboto,sans-serif] text-base font-normal text-[#333333] cursor-pointer min-w-[109px] h-8 w-[109px] rounded-[999px] border-[none]"
-                    >
-                      Full time
-                    </button>
-                    <button
-                      class="bg-[rgba(51,51,51,0.04)] [font-family:Roboto,sans-serif] text-base font-normal text-[#333333] cursor-pointer min-w-[149px] h-8 w-[149px] ml-4 rounded-[999px] border-[none]"
-                    >
-                      +2 Experience
-                    </button>
-                  </div>
-                </a>
-                <a href="./career-details.html" class="career-box-content cursor-pointer border bg-[white] box-border flex justify-center items-stretch flex-col gap-4 max-w-[600px] px-[31px] py-8 rounded-lg border-solid border-[#e8e8e8]"
-                >
-                  <p
-                    class="[font-family:Montserrat,sans-serif] text-[35px] font-semibold leading-[42px] uppercase text-[#333333]"
-                  >
-                    product manager
                   </p>
                   <p
                     class="[font-family:'Open_Sans',sans-serif] text-[17px] font-normal text-left leading-[23px] text-[#8e8e8e]"
