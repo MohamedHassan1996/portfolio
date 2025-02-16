595b6f08232c93aa105816b6bcbb74a6cf07d7d8
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-02-16 14:59:13 +0200

    activate career

--- resources/views/Emails/send_candidate_cv.blade.php
+++ resources/views/Emails/send_candidate_cv.blade.php
@@ -46,7 +46,6 @@
         <p><strong>Name:</strong> {{ $candidate->name }}</p>
         <p><strong>Email:</strong>{{  $candidate->email }}</p>
         <p><strong>Phone:</strong>{{  $candidate->phone }}</p>
-@dd($candidate->phone)
     </div>
 </div>
 
