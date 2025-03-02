9bede254090d43876913ab7a499d046faee869f7
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-03-02 11:07:00 +0200

    test

--- resources/views/Blog/index.blade.php
+++ resources/views/Blog/index.blade.php
@@ -68,7 +68,7 @@
                                     <div
                                         class="bg-[white] box-border relative flex justify-start flex-col rounded-xl border-2 border-solid border-[#e8e8e8]">
                                         <img src="{{ url("storage/$blog->thumbnail") }}"
-                                            class="w-full h-[400px] rounded-lg" alt="image1" loading="lazy">
+                                            class="w-full h-full rounded-lg" alt="image1" loading="lazy">
                                         <div
                                             class="bg-cover bg-no-repeat  box-border flex justify-start items-center flex-col pt-4">
                                             <div
@@ -84,7 +84,7 @@
                                         <div
                                             class="box-border flex justify-center items-stretch flex-col pt-11 pb-6 px-[23px]">
                                             <h2
-                                                class="[font-family:Montserrat,sans-serif] text-[35px] font-semibold leading-[42px] text-[#333333] p-0 m-0 w-full sm:w-[600px] sm-max-w-[650px]">
+                                                class="[font-family:Montserrat,sans-serif] text-[35px] font-semibold leading-[42px] text-[#333333] p-0 m-0 w-full">
                                                 {{ $blog->title }}
                                             </h2>
                                             <p
@@ -136,7 +136,7 @@
                                                 class="rounded bg-[rgba(51,51,51,0.04)] box-border flex justify-between items-center flex-row gap-2 h-[43px] px-[15px]">
                                                 <p
                                                     class="[font-family:Roboto,sans-serif] text-lg font-medium text-[#333333] m-0 p-0">
-                                                    {{ $blogCategory->name }}
+                                                    {{ Str::limit($blogCategory->name, 30) }}
                                                 </p>
                                                 <p
                                                     class="[font-family:Roboto,sans-serif] text-lg font-medium text-[#333333] m-0 p-0">
